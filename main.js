954f787a4ef4b749306aedbcd91c54417f0ae15a
Joshua Bamidele <exploits4real@gmail.com>
2024-04-26 21:03:00 +0100

    Stage 1

---
+++ main.js
@@ -0,0 +1,6 @@
+//change navbar stlyes on scroll
+
+   window.addEventListener('scroll', ()=>{
+        document.querySelector('nav').classList.toggle
+        ('window-scroll', window.scrollY > 0)
+   })
