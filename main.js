6bc8bb5133b121fc194afee46ac40b2604073522
Joshua Bamidele <exploits4real@gmail.com>
2024-04-29 00:00:19 +0100

    new

--- main.js
+++ main.js
@@ -4,3 +4,13 @@
         document.querySelector('nav').classList.toggle
         ('window-scroll', window.scrollY > 0)
    })
+
+   //show/dide//
+
+   const menu = document.querySelector(".nav_menu")
+   const menuBtn = document.querySelector("#open-menu-btn")
+   const closedn = document.querySelector("#Close-menu-btn");
+
+   menuBtn.addEventListener('click', ()=>{
+     menu.style.display = 'block'
+   }) 
